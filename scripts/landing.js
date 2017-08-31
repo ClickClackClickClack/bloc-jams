38b60e8230e40abc29e8a9b9a4b60b6e868d723e
Jon Reck <jonathonreck@gmail.com>
2017-08-31 15:12:11 -0400

    checkpoint 10 collection

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,21 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+
+     point.style.opacity = 1;
+     point.style.transform = "scaleX(1) translateY(0)";
+     point.style.msTransform = "scaleX(1) translateY(0)";
+     point.style.WebkitTransform = "scaleX(1) translateY(0)";
+ };
  var animatePoints = function(points) {
 
-   var revealPoint = function(value) {
-        points[value].style.opacity = 1;
-        points[value].style.transform = "scaleX(1) translateY(0)";
-        points[value].style.msTransform = "scaleX(1) translateY(0)";
-        points[value].style.WebkitTransform = "scaleX(1) translateY(0)";
+     forEach(points, revealPoint);
 
-  }
-  for(var i = 0; i <  points.length; i++) {
+  };
+  /*for(var i = 0; i <  points.length; i++) {
     revealPoint(i);
-  }
- };
+  }*/
+
  window.onload = function () {
    if (window.innerHeight > 950) {
          animatePoints(pointsArray);
