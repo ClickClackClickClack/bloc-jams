eacc169efa331de99abff90e2ef865c5d3d155bf
Jon Reck <jonathonreck@gmail.com>
2017-09-01 15:30:36 -0400

    checkpoint 8 revised

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(point) {
-
+     console.log(point);
      point.style.opacity = 1;
      point.style.transform = "scaleX(1) translateY(0)";
      point.style.msTransform = "scaleX(1) translateY(0)";
      point.style.WebkitTransform = "scaleX(1) translateY(0)";
  };
  var animatePoints = function(points) {
-
-     forEach(points, revealPoint);
-
+    for(var i = 0; i <  points.length; i++) {
+        revealPoint(points[i]);
+    }
   };
-  /*for(var i = 0; i <  points.length; i++) {
-    revealPoint(i);
-  }*/
-
  window.onload = function () {
    if (window.innerHeight > 950) {
          animatePoints(pointsArray);
