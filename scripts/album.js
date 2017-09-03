47045f96361d3d046671542e66a0440e0dc9a215
Jon Reck <jonathonreck@gmail.com>
2017-09-03 10:05:04 -0400

    reset assignment 11 js

--- scripts/album.js
+++ scripts/album.js
@@ -12,6 +12,7 @@ var albumPicasso = {
          { title: 'Magenta', duration: '2:15'}
      ]
  };
+
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -26,6 +27,22 @@ var albumPicasso = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+
+ /*var albumKing = {
+     title: 'The Thrillers',
+     artist: 'Stephen King',
+     label: 'Scary Stuff',
+     year: '1990',
+     albumArtUrl: 'assets/images/album_covers/19.png',
+     songs: [
+         { title: 'Carrie', duration: '4:11' },
+         { title: 'It', duration: '5:05' },
+         { title: 'The Shining', duration: '1:21'},
+         { title: 'Misery', duration: '9:54' },
+         { title: 'The Stand', duration: '2:42'}
+     ]
+ };*/
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -37,8 +54,9 @@ var albumPicasso = {
 
      return template;
  };
+ //var albumImage = document.getElementsByClassName('album-cover-art')[0];
  var setCurrentAlbum = function(album) {
-   var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
@@ -56,3 +74,12 @@ var albumPicasso = {
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
  };
+ /*var albumList = [albumPicasso, albumMarconi, albumKing];
+ var i = 0;
+ albumImage.addEventListener("click", function(){
+   setCurrentAlbum(albumList[i]);
+   i++;
+   if (i == albumList.length) {
+     i = 0;
+   }
+ });*/
